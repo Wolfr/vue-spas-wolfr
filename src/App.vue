0ff44349c468edccdd1255ee6969087e61defedc
Johan Ronsse <johan.ronsse@gmail.com>
2020-05-22 13:23:18 +0200

    First commit (part 1: preparing your app)

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Navigation/>
+    <router-view class="container" />
+  </div>
+</template>
+
+<style lang="scss">
+  $primary: #146136;
+  @import "node_modules/bootstrap/scss/bootstrap";
+</style>
+
+<script>
+  import Navigation from '@/components/Navigation.vue';
+
+  export default {
+    name: "app",
+    components: {
+      Navigation,
+    },
+  }
+
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Meetings from '../views/Meetings.vue'
+import Register from '../views/Register.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/meetings',
+    name: 'meetings',
+    component: Meetings
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '*',
+    redirect: '/'
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
